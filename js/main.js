ce2a35a5495afaaea560709e390c4ffc00bf35c5
urbanoluigi <urrbanoluigi@gmail.com>
2023-11-14 15:03:11 +0100

    added preventdefault

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 //alert di default che blocca il sito (eseguito subito)
+
+alert("Benvenuto nella pagina dei biglietti,compila tutti i dati richiesti per avere il calcolo del prezzo del biglietto.");
+
+// vediamo se funge l'alert come deve
+
+console.log("Benvenuto")
+
+//tutto funziona come deve, per ora
+
+//per non perdere i dati dell'utente mettiamo questo comando così
+//la pagina non ci fa perdere i dati
+
+function utentehafinito(event){
+    event.preventDefault();
+}
