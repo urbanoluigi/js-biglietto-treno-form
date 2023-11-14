9ab264f4b2df406c759c816b717321302db57071
urbanoluigi <urrbanoluigi@gmail.com>
2023-11-14 18:09:47 +0100

    updated

--- js/main.js
+++ js/main.js
@@ -19,6 +19,11 @@ let name = document.getElementById("name_and_surname").value;
 let km= document.getElementById("kmpercorrere").value;
 let age = document.getElementById("age").value;
 
+document.getElementById("name_surname").innerHTML = name;
+document.getElementById("final-price").innerHTML = name;
+document.getElementById("name_surname").innerHTML = name;
+
+
     let prezzoBase = kmDaPercorrere * 0.21;
 
 // Applica gli sconti in base all'età
